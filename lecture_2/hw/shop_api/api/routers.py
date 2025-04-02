d3510bcbe2af7b10ada870f1c0e4e7891e627a6e
Denis <den12262@mail.ru>
2025-04-02 12:22:25 +0300

    hw-2: added last router

--- lecture_2/hw/shop_api/api/routers.py
+++ lecture_2/hw/shop_api/api/routers.py
@@ -19,7 +19,7 @@ def generate_id_carts(carts: dict):
     return max(carts.keys(), default=0) + 1
 
 
-def generate_id_keys(carts: dict):
+def generate_id_item(carts: dict):
     return max(items.keys(), default=0) + 1
 
 
@@ -85,9 +85,9 @@ async def get_cart_param(
 @router_cart.post("/{cart_id}/add/{item_id}", status_code=HTTPStatus.OK)
 async def add_item_in_cart(cart_id: int, item_id: int):
     if cart_id not in carts:
-        return HTTPException(status_code=404, detail="cart id not found")
+        raise HTTPException(status_code=404, detail="cart id not found")
     if item_id not in items:
-        return HTTPException(status_code=404, detail="item id not found")
+        raise HTTPException(status_code=404, detail="item id not found")
 
     cart = carts[cart_id]
     item = items[item_id]
@@ -107,3 +107,91 @@ async def add_item_in_cart(cart_id: int, item_id: int):
     cart.price += item.price
 
     return {"message": "Item added to cart"}
+
+
+@router_item.post("", status_code=HTTPStatus.CREATED)
+async def create_item(item: dict):
+    id_item = generate_id_item()
+    items[id_item] = Items(id=id_item, name=item["name"], price=item["price"])
+
+    return items[id_item]
+
+
+@router_item.get("/{id}", status_code=HTTPStatus.OK)
+async def get_item(id: int):
+    if id not in items:
+        raise HTTPException(status_code=404, detail="Not found id")
+    if items[id].deleted == True:
+        raise HTTPException(status_code=404, detail="Items has been deleted")
+    return items[id]
+
+
+@router_item.get("", status_code=HTTPStatus.OK)
+async def get_list_items(
+    offset: NonNegativeInt = 0,
+    limit: PositiveInt = 10,
+    min_price: NonNegativeFloat = None,
+    max_price: NonNegativeFloat = None,
+    show_deleted: bool = False,
+):
+    if not items:
+        raise HTTPException(status_code=404, detail="Items is empty")
+
+    items_val = list(items.values())
+
+    if show_deleted:
+        items_val = [elem for elem in items_val if not elem.deleted]
+    if min_price is not None:
+        items_val = [elem for elem in items_val if elem.price >= min_price]
+    if max_price is not None:
+        items_val = [elem for elem in items_val if elem.price <= max_price]
+
+    items_val = items_val[offset : offset + limit]
+
+    return items_val
+
+
+@router_item.put("/{id}", status_code=HTTPStatus.OK)
+async def put_items(id: int, item: dict):
+    if id not in items:
+        raise HTTPException(status_code=404, detail="id not in items")
+    items[id] = Items(id=id, name=item["name"], price=item["price"])
+    return items[id]
+
+
+@router_item.patch("/{id}", status_code=HTTPStatus.OK)
+async def put_items(id: int, item: dict):
+    if id not in items:
+        raise HTTPException(status_code=404, detail="id not in items")
+
+    old_item = items[id]
+
+    if old_item.status_delete == True:
+        raise HTTPException(status_code=304, detail="Item not modified")
+
+    invalid_keys = set(item.keys()) - set(old_item.model_dump().keys())
+
+    if invalid_keys:
+        raise HTTPException(status_code=422, detail="Wrong field")
+
+    # pattern for update data
+    name = item.get("name", old_item.name)
+    price = item.get("price", old_item.price)
+    deleted = item.get("deleted", old_item.delete)
+
+    items[id] = Items(id=id, name=name, price=price, deleted=deleted)
+
+    if old_item.deleted != items[id].deleted:
+        raise HTTPException(status_code=422, detail="status connot be changed")
+
+    return items[id]
+
+
+@router_item.delete("/{id}", status_code=HTTPStatus.OK)
+async def delete_it(id: int):
+    if id not in items:
+        raise HTTPException(status_code=404, detail="id not in items")
+
+    items[id].deleted = True
+
+    return items[id]
