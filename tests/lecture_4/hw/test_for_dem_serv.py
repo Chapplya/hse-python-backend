3c7c433bc695b24fbb3165d1b9bd05c1de9b20a6
Denis <den12262@mail.ru>
2025-04-14 20:52:45 +0300

    hw-4: redone test with model_dump

--- tests/lecture_4/hw/test_for_dem_serv.py
+++ tests/lecture_4/hw/test_for_dem_serv.py
@@ -4,7 +4,7 @@ from faker import Faker
 from pydantic import SecretStr
 import pytest
 from fastapi.testclient import TestClient
-
+import factory
 from hw_4.demo_service.api.main import create_app
 from hw_4.demo_service.api.utils import initialize
 from lecture_4.demo_service.api.contracts import UserResponse
@@ -19,6 +19,29 @@ app = create_app()
 
 
 fake = Faker()
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = dict
+
+    username = factory.Faker("user_name")
+    name = factory.Faker("name")
+    birthdate = str(fake.date_time().isoformat())
+    password = factory.Faker("password")
+
+
+@pytest.fixture()
+def create_user(client):
+    resp = client.post("/user-register", json=UserFactory.build(password="12345678910"))
+    json = resp.json()
+    return UserResponse(
+        uid=json["uid"],
+        username=json["username"],
+        name=json["name"],
+        birthdate=json["birthdate"],
+        role=json["role"],
+    )
 
 
 @pytest.fixture()
@@ -44,55 +67,17 @@ def client():
             app.state.user_service._last_id = 0
 
 
-@pytest.fixture()
-def user(client):
-    username = "test_user_1"
-
-    name = "test_name_1"
-    birthdate = str(fake.date_time().isoformat())
-    password = "12345678910"
-    resp = client.post(
-        "/user-register",
-        json={
-            "username": username,
-            "name": name,
-            "birthdate": birthdate,
-            "password": password,
-        },
-    )
-    json = resp.json()
-    return UserResponse(
-        uid=json["uid"],
-        username=username,
-        name=name,
-        birthdate=birthdate,
-        role=json["role"],
-    )
-
-
 def test_correct_register(client):
     response = client.post(
-        "/user-register",
-        json={
-            "username": "Vadik",
-            "name": "Vadim",
-            "birthdate": "2000-10-10",
-            "password": "123456789",
-        },
+        "/user-register", json=UserFactory.build(password="1234567890")
     )
     assert response.status_code == HTTPStatus.OK
 
 
-def test_fail_register_usename(client, user):
+def test_fail_register_usename(client):
     response = client.post(
         "/user-register",
-        json={
-            "username": user.username,
-            "name": "Test User New",
-            "birthdate": "2000-01-01",
-            "role": "USER",
-            "password": "1234567899",
-        },
+        json=UserFactory.build(password="123"),
     )
 
     assert response.status_code == HTTPStatus.BAD_REQUEST
@@ -113,16 +98,24 @@ def test_get_by_username(client):
 @pytest.mark.parametrize(
     "parameters,auth_name, auth_passw, excpected_code",
     [
-        ({"id": 10, "username": "test_user"},"kaban", "123456789",  HTTPStatus.BAD_REQUEST),
-        ({},"kaban", "123456789", HTTPStatus.BAD_REQUEST),
-        ({"username": "unnreal_name"}, "kaban", "123456789",HTTPStatus.NOT_FOUND ),
-        ({"id": 1},"kaban", "12345678900", HTTPStatus.UNAUTHORIZED)
+        (
+            {"id": 10, "username": "test_user"},
+            "kaban",
+            "123456789",
+            HTTPStatus.BAD_REQUEST,
+        ),
+        ({}, "kaban", "123456789", HTTPStatus.BAD_REQUEST),
+        ({"username": "unnreal_name"}, "kaban", "123456789", HTTPStatus.NOT_FOUND),
+        ({"id": 1}, "kaban", "12345678900", HTTPStatus.UNAUTHORIZED),
     ],
 )
-def test_error_none_params_user_get(client, parameters, auth_name, auth_passw, excpected_code):
+def test_error_none_params_user_get(
+    client, parameters, auth_name, auth_passw, excpected_code
+):
     response = client.post("/user-get", params=parameters, auth=(auth_name, auth_passw))
 
     assert response.status_code == excpected_code
+
 
 def test_get_usename(client):
     response = client.post(
@@ -132,16 +125,19 @@ def test_get_usename(client):
     assert response.status_code == HTTPStatus.OK
     data = response.json()
 
-    assert data == {"uid": 1, "username": "kaban", "name": "papa", "role": "admin", 'birthdate': '2001-12-10T00:00:00'}
+    assert data == {
+        "uid": 1,
+        "username": "kaban",
+        "name": "papa",
+        "role": "admin",
+        "birthdate": "2001-12-10T00:00:00",
+    }
+
 
 @pytest.mark.parametrize(
-    
     "param, excepted_code",
-    [
-        ({"id": 1}, HTTPStatus.OK),
-        ({"id": 52}, HTTPStatus.BAD_REQUEST)
-    ]
-)   
+    [({"id": 1}, HTTPStatus.OK), ({"id": 52}, HTTPStatus.BAD_REQUEST)],
+)
 def test_user_promote(client, param, excepted_code):
     response = client.post(
         "/user-promote",
@@ -151,8 +147,8 @@ def test_user_promote(client, param, excepted_code):
     assert response.status_code == excepted_code
 
 
-def test_fail_403_promote_user_without_admin(client, user):
-    print(user.username)
+def test_fail_403_promote_user_without_admin(client, create_user):
+    user = create_user
     response = client.post(
         "/user-promote",
         params={"id": user.uid},
@@ -167,9 +163,17 @@ async def test_initialize():
     async with initialize(app):
         user_service = app.state.user_service
         admin = user_service.get_by_username("admin")
+        expected_data = {
+            "uid": 1,
+            "info": {
+                "username": "admin",
+                "name": "admin",
+                "role": "admin",
+                "password": str(SecretStr("superSecretAdminPassword123")),
+                "birthdate": "1970-01-01T03:00:00",
+            },
+        }
 
-        assert admin.uid == 1
-        assert admin.info.username == "admin"
-        assert admin.info.name == "admin"
-        assert admin.info.role == UserRole.ADMIN
-        assert admin.info.password == SecretStr("superSecretAdminPassword123")
+        admin_data = admin.model_dump(mode="json")
+
+        assert admin_data == expected_data
